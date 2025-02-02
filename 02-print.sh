69ec7f0a8a0ecc547453b4b3b2ba8f01dce1652e
Q Dev <qdev007@gmail.com>
2025-02-01 23:29:26 -0500

    Sample Test File

--- 02-print.sh
+++ 02-print.sh
@@ -1,5 +1,28 @@
 # Print some message on the screen using echo (cam also use printf)
 
-#echo input
+# syntax echo input
 
 echo "Hello World"
+
+# print colors
+# syntax echo -e "\e[COLm Hello World\e[0m"
+# -e - enable colors
+# \e[COLm - choose color to use
+# \e[0m - disable colors ; 0 means disable
+
+# Color codes
+# Color     - Code
+# RED       - 31
+# GREEN     - 32
+# YELLOW    - 33
+# BLUE      - 34
+# MAGENTA   - 35
+# CYAN      - 36
+
+echo -e "\e[31m Hello World in Red\e[0m"
+echo -e "\e[32m Hello World in Green\e[0m"
+echo -e "\e[32m Hello World in Yellow\e[0m"
+echo -e "\e[34m Hello World in Blue\e[0m"
+echo -e "\e[35m Hello World in Magenta\e[0m"
+echo -e "\e[36m Hello World in Cyan\e[0m"
+
