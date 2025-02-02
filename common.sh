fa811fffc31ad6438b0f5ad8bf0faf5e071a55e0
Q Dev <qdev007@gmail.com>
2025-02-02 01:02:22 -0500

    Testing Common Variables

--- common.sh
+++ common.sh
@@ -1,4 +1,4 @@
-# holds variables that are common across multiple scripts
+# holds variables and functions that are common across multiple scripts
 
 # to load variables here in other scripts use load command at top of script e.g., load common.sh
 
